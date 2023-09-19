ebd97c81120136e266cd825b9ce4e909be2a6f25
wanderer-s <ows0871@gmail.com>
2023-09-19 17:21:10 +0900

    project init

---
+++ src/main/kotlin/com/assignment/remit/RemitApplication.kt
@@ -0,0 +1,11 @@
+package com.assignment.remit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RemitApplication
+
+fun main(args: Array<String>) {
+    runApplication<RemitApplication>(*args)
+}
---
+++ src/test/kotlin/com/assignment/remit/RemitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.assignment.remit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RemitApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
