fb0f269ab4881d9a7684656a95e008a76d2431b9
wanderer-s <ows0871@gmail.com>
2023-09-21 23:34:46 +0900

    Local DB를 위한 Docker-compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("com.palantir.docker-compose") version "0.35.0"
     kotlin("jvm") version "1.8.22"
     kotlin("kapt") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
